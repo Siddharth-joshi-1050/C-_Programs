ede2ed24b44e0bec0e6e89fdc01a2a5e56c6083c
Siddharth-joshi-1050 <sidjoshi2198@gmail.com>
2020-03-08 10:50:35 -0700

    Defined function for inverse kinematics for 3R planar robot

--- robot.h
+++ robot.h
@@ -112,6 +112,62 @@ bool Robot::intersection(double circle_x, double circle_y, double circle_r)
 
 void Robot::inverse_kin(double end_pos_x, double end_pos_y, double end_pos_theta, double linklength0, double linklength1, double linklength2)
 {
+    end_pos_theta *= M_PI / 180;
+    this->end_pos_x = end_pos_x;
+    this->end_pos_y = end_pos_y;
+    this->end_pos_theta = end_pos_theta;
 
+    double x, y, c2, s21, s22, theta21, theta22, s11, c11, s12, c12, theta11, theta12, theta31, theta32;
+
+    x = end_pos_x - (linklength2*cos(end_pos_theta));
+    y = end_pos_y - (linklength2*sin(end_pos_theta));
+    c2 = (pow(x,2) + pow(y,2) - pow(linklength0,2) - pow(linklength1,2))/(2 * linklength0 * linklength1);
+    s21 = pow(double(1 - pow(c2,2)), 0.5);
+    s22 = s21 * -1;
+    theta21 = atan(s21/c2);
+    theta22 = atan(s22/c2);
+
+    s11 = ((linklength0 + (linklength1*c2))*y - (linklength1*s21*x))/(pow(x,2) + pow(y,2));
+    c11 = ((linklength0 + (linklength1*c2))*x - (linklength1*s21*y))/(pow(x,2) + pow(y,2));
+
+    s12 = ((linklength0 + (linklength1*c2))*y - (linklength1*s22*x))/(pow(x,2) + pow(y,2));
+    c12 = ((linklength0 + (linklength1*c2))*x - (linklength1*s22*y))/(pow(x,2) + pow(y,2));
+
+    theta11 = atan(s11/c11);
+    theta12 = atan(s12/c12);
+
+
+    theta31 = end_pos_theta - theta21 - theta11;
+    theta32 = end_pos_theta - theta22 - theta12;
+
+    cout << "Trial Outputs: " << endl;
+    cout << "X : "<< x << endl;
+    cout << "Y : "<< y << endl;
+    cout << "c2 : "<< c2 << endl;
+    cout << "s21 : " << s21 << endl;
+    cout << "s22 : " << s22 << endl; 
+    cout << "theta 21 :" << (theta21*180)/M_PI << endl;
+    cout << "theta 22 :" << (theta22*180)/M_PI << endl;
+    cout << "theta 11 :" << (theta11*180)/M_PI << endl;
+    cout << "theta 12 :" << (theta12*180)/M_PI << endl;
+    cout << "theta 31 :" << (theta31*180)/M_PI << endl;
+    cout << "theta 32 :" << (theta32*180)/M_PI << endl;
+
+
+    cout << "=======================================================" << endl;
+    cout<< "The first set of feasible solution is: "<< endl;
+    cout << "=======================================================" << endl;
+
+    cout << "Theta 1 : " << (theta11*180)/M_PI  << " degrees " << endl;
+    cout << "Theta 2 : " << (theta21*180)/M_PI <<  " degrees " << endl;
+    cout << "Theta 3 : " << (theta31*180)/M_PI <<  " degrees " << endl;
+
+    cout << "=======================================================" << endl;
+    cout<< "The second set of feasible solution is: "<< endl;
+    cout << "=======================================================" << endl;
+    
+    cout << "Theta 1 : " << (theta12*180)/M_PI  << " degrees " << endl;
+    cout << "Theta 2 : " << (theta22*180)/M_PI <<  " degrees " << endl;
+    cout << "Theta 3 : " << (theta32*180)/M_PI <<  " degrees " << endl;
 }
 
